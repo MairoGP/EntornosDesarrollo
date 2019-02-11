eabcfc57f3c9a365d9f95455d20074df108acf1e
Mario Garcia <mario_onlysk8@hotmail.com>
2019-02-11 19:42:48 +0100

    Probando

--- CuatroRuedas/src/Car.java
+++ CuatroRuedas/src/Car.java
@@ -5,11 +5,11 @@ public class Car extends Vehicle{
     {
         super(4,c,1000,210);
     }
-    
+     
     public void accelerate()
     {
         System.out.println("The car is accelerating.");
-    }
+    } 
     public void brk()
     {
         System.out.println("The car is braking.");
